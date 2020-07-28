820c7547def1f95b41a8c94415125378df874076
ruanvictorreis <ruanvictorreis@gmail.com>
2020-07-28 18:34:47 -0300

    First commit

---
+++ MarvelCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Ruan Reis on 28/07/20.
+//  Copyright © 2020 Ruan Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

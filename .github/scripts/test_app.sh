aae4dee80af8ed988d6408d4ba68634de3644cb5
ruanvictorreis <ruanvictorreis@gmail.com>
2020-10-12 19:10:16 -0300

    Fixing xcodebuild test command

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace Marvel.xcworkspace \
-            -scheme MAPP \
+            -scheme Marvel \
             -destination platform=iOS\ Simulator,OS=13.7,name=iPhone\ 11 \
             clean test | xcpretty

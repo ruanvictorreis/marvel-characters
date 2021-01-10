18f9eb46269db48b66fe886d7ebeee62496a1906
ruanvictorreis <ruanvictorreis@gmail.com>
2021-01-10 02:21:20 -0300

    Changing iOS version

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace Marvel.xcworkspace \
             -scheme Marvel \
-            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=14.2,name=iPhone\ 11 \
             clean test | xcpretty

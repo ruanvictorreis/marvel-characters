241cd60609ac47530e0860d7fd87b6578e73bfc3
ruanvictorreis <ruanvictorreis@gmail.com>
2021-02-08 22:33:37 -0300

    Including json files to mock api

--- MarvelTests/Source/Workers/CharacterList/Mocks/CharacterListWorkerMock.swift
+++ MarvelTests/Source/Workers/CharacterList/Mocks/CharacterListWorkerMock.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Ruan Reis. All rights reserved.
 //
 
+import Foundation
 @testable import Marvel
 
 class CharacterListWorkerSuccessMock: CharacterWorkerProtocol {
@@ -29,25 +30,57 @@ class CharacterListWorkerSuccessMock: CharacterWorkerProtocol {
     func fetchCharacterList(offset: Int,
                             sucess: @escaping CharacterWorkerSuccess,
                             failure: @escaping CharacterWorkerError) {
-        
-        let response = CharacterListResponseMock
-            .build(offset: offset, pageCount: 20)
-        
-        sucess(response)
+        do {
+            let data = readFile(resource: "CharacterList") ?? Data()
+            var response = try JSONDecoder().decode(
+                CharacterListResponse.self, from: data)
+            
+            let limit = offset + 20
+            let total = response.data.results.count
+            let endPage = limit < total ? limit : total
+            let characters = response.data.results
+            let results = Array(characters[offset..<endPage])
+            
+            response.data.offset = offset
+            response.data.limit = limit
+            response.data.total = total
+            response.data.count = results.count
+            response.data.results = results
+            sucess(response)
+        } catch {
+            failure(nil)
+        }
     }
     
     func fetchCharacterList(searchParameter: String, offset: Int,
                             sucess: @escaping CharacterWorkerSuccess,
                             failure: @escaping CharacterWorkerError) {
-        
-        var response = CharacterListResponseMock
-            .build(offset: offset, pageCount: 20)
-        
-        let results = response.data.results
-            .filter({ $0.name.contains(searchParameter) })
-        
-        response.data.results = results
-        sucess(response)
+        do {
+            let data = readFile(resource: "CharacterSearch") ?? Data()
+            var response = try JSONDecoder().decode(
+                CharacterListResponse.self, from: data)
+            
+            let limit = offset + 20
+            let total = response.data.results.count
+            let endPage = limit < total ? limit : total
+            let characters = response.data.results
+            let results = Array(characters[offset..<endPage])
+            
+            response.data.offset = offset
+            response.data.limit = limit
+            response.data.total = total
+            response.data.count = results.count
+            response.data.results = results
+            sucess(response)
+        } catch {
+            failure(nil)
+        }
+    }
+    
+    private func readFile(resource: String) -> Data? {
+        let bundle = Bundle(for: type(of: self))
+        let filePath = bundle.path(forResource: resource, ofType: "json")
+        return try? String(contentsOfFile: filePath ?? "").data(using: .utf8)
     }
 }
 
@@ -62,7 +95,7 @@ class CharacterListWorkerFailureMock: CharacterWorkerProtocol {
     }
     
     func deleteFavorite(character: Character, sucess: Completation?, failure: Completation?) {
-       failure?()
+        failure?()
     }
     
     func fetchCharacterList(offset: Int,
